de51c55db2c2fe755b751ec173dc07bf14deb4db
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2020-05-15 17:05:58 +0200

    Written a shell script to run the BIBtex file first before the pdflatex compiler and output a status of the compilation

---
+++ compile.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ "$#" -gt 2 ]; then
+    echo "Usage: ./compile.sh <FILENAME>"
+    exit -1;
+fi
+
+if [ "$#" -eq 0 ]; then
+    FILE=finalReport
+fi
+
+if [ "$#" -eq 1 ]; then
+    FILE="$1"
+fi
+FILEWITHEX=${FILE}".tex"
+PDFLATEXFLAGS="-halt-on-error"
+
+echo "Compiling the $FILEWITHEX using PDFlatex"
+
+runBibTex="bibtex ${FILE}"
+runpdfLatex="pdflatex ${FILEWITHEX} ${PDFLATEXFLAGS}"
+
+outputBib=$(eval ${runBibTex})
+outputPdf=$(eval ${runpdfLatex})
+
+testError=$(echo $outputPdf | grep -ci "error") #>/dev/null
+#echo "$testError"
+#runtestError=$(eval ${testError})
+
+if [ $testError -gt 0 ]; then
+    echo "PDF Generation Failed"
+else
+    echo "PDF Generation Success"
+fi
